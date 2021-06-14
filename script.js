9ad08f712b18c4ec40b36b008e726d551d93b813
Alex Chernov <alexusblack@gmail.com>
2021-06-14 13:03:27 +1000

    auto reset timer

--- script.js
+++ script.js
@@ -11,12 +11,19 @@
     const removePooBtn = document.querySelector('.value__remove--poo');
     const removeWeeBtn = document.querySelector('.value__remove--wee');
 
+    let date = parseInt(getSaveVal('date', getNextMidnight()));
+    saveVal('date', date);
     let bowelMovements = {
         poos: getSaveVal('poos', 0),
         wees: getSaveVal('wees', 0),
         yestPoos: getSaveVal('yest-poos', 0),
         yestWees: getSaveVal('yest-wees', 0)
     };
+
+    if(Date.now() > date) {
+        midnightSwitch();
+    }
+    renderTimeToMidnightSwitch();
 
     renderPooAndWee();
     yestPooValue.textContent = bowelMovements['yestPoos'];
@@ -26,6 +33,37 @@
     addWeeBtn.addEventListener('click', () => addBowelMovement('wees', 1));
     removePooBtn.addEventListener('click', () => addBowelMovement('poos', -1));
     removeWeeBtn.addEventListener('click', () => addBowelMovement('wees', -1));
+
+    setInterval(function() {
+        if(Date.now() > date) {
+            midnightSwitch();
+        }
+        renderTimeToMidnightSwitch();
+    }, 5000);
+
+    function renderTimeToMidnightSwitch() {
+        const timeLeftUnix = date - Date.now();
+        const timeLeft = new Date(timeLeftUnix);
+        // let floatHours = timeLeft / 60 / 60;
+        // let intHours = parseInt(floatHours);
+        // let minutes = (floatHours - intHours) * 60;
+        resetTimeValue.textContent = timeLeft.getUTCHours() + ':' + timeLeft.getUTCMinutes();
+    }
+
+    function midnightSwitch() {
+        saveVal('poos', 0);
+        saveVal('wees', 0);
+        saveVal('yest-poos', bowelMovements['poos']);
+        saveVal('yest-wees', bowelMovements['wees']);
+        localStorage.removeItem('date');
+        document.location.reload();
+    }
+
+    function getNextMidnight() {
+        const m = new Date();
+        m.setHours(24,0,0,0); // next midnight
+        return m.getTime();
+    }
 
     function addBowelMovement(type, value) {
         bowelMovements[type] = parseInt(bowelMovements[type]) + value;
